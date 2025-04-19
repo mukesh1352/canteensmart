7f10b1720c749c298bf12a4008024b6a335a8975
mukesh <mukeshnarravula14@gmail.com>
2025-04-19 18:58:02 +0530

    did the page

--- smartcanteen/src/app/page.tsx
+++ smartcanteen/src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Image from "next/image";
 import Header from "./components/header";
 import ItemsDisplay from "./components/items";
+import AnalyticsPage from "./components/analytics";
 export default function Home() {
   return (
     <>
       <Header />
       <ItemsDisplay />
+      <AnalyticsPage />
     </>
   );
 }
