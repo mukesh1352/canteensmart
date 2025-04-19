86f490bc24f9cc1e973abc3dc412a20489e64829
mukesh <mukeshnarravula14@gmail.com>
2025-04-19 19:01:05 +0530

    finished the admin side changes

--- smartcanteen/src/app/page.tsx
+++ smartcanteen/src/app/page.tsx
@@ -1,13 +1,11 @@
 // import Image from "next/image";
 import Header from "./components/header";
 import ItemsDisplay from "./components/items";
-import AnalyticsPage from "./components/analytics";
 export default function Home() {
   return (
     <>
       <Header />
       <ItemsDisplay />
-      <AnalyticsPage />
     </>
   );
 }
